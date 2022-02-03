e051d2208f6aed97676cd11b7978be4104e4f3cb
Bohdan Vakhmistrov <74067203+krotosua@users.noreply.github.com>
2022-02-03 10:31:10 +0300

    Add files via upload

---
+++ Clock_Project/server/controllers/masterController.js
@@ -0,0 +1,24 @@
+const {Master, City}= require('../models/models')
+const ApiError = require('../error/ApiError')
+const masterLogic=require('../BissnesLogic/masterLogic')
+class MasterController{
+    async create(req,res,next){
+      await masterLogic.create(req,res,next)
+    }
+
+    async getAll(req,res,next){
+        await masterLogic.getAll(req,res,next)
+    }
+    async getOne(req,res,next){
+        await masterLogic.getOne(req,res,next)
+    }
+    async update(req,res,next){
+        await masterLogic.update(req,res,next)
+    }
+    async deleteOne(req,res){
+        await masterLogic.deleteOne(req,res)
+    }
+
+}
+
+module.exports = new MasterController()
